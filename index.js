773cef12aa195298ec5ba46a2bfff2f8c39b2390
cui wang <mrcui422@gmail.com>
2017-07-14 17:54:56 +0800

    Demo

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
+//app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
+app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/public/'));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+})
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
+/*
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+*/
+
+const addon = require('./build/Release/factorial');
+
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
+server.listen(port);
+
+io.on('connection', function( client ){
+    onClientConnect( client );
+    client.on("disconnect", onClientDisconnect);
+    client.on("request", onGetResult);
+});
+
+
+onClientConnect = function( client ){
+    console.log('connected');
+};
+
+onClientDisconnect = function(){
+    console.log('disconnected');
+};
+
+onGetResult = function(value) {
+	client = this
+	console.log("received: ", value);
+	client.emit('get_result', addon.factorial(parseInt(value)));
+}
 
 
