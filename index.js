d0bea82ba0569d2122851ce7c1c514a40f59f4ed
cui wang <mrcui422@gmail.com>
2017-07-14 18:16:17 +0800

    Demo

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
+//include c++ addon
+///build/Release/factorial.node
+var addon = require('./build/Release/factorial');
 var express = require('express');
-var app = express();
 
 var port = process.env.PORT || 5000;
-//app.set('port', (process.env.PORT || 5000));
 
+var app = express();
 app.use('/', express.static(__dirname + '/'));
 app.use('/', express.static(__dirname + '/public/'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 })
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-/*
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-*/
-
-const addon = require('./build/Release/factorial');
 
 var http = require('http');
 var server = http.Server(app);
@@ -33,7 +21,7 @@ server.listen(port);
 io.on('connection', function( client ){
     onClientConnect( client );
     client.on("disconnect", onClientDisconnect);
-    client.on("request", onGetResult);
+    client.on("request", onRequest);
 });
 
 
@@ -45,10 +33,11 @@ onClientDisconnect = function(){
     console.log('disconnected');
 };
 
-onGetResult = function(value) {
-	var client = this
+//response the result of factorial to request from client.
+onRequest = function(value) {
+	var client = this;
 	console.log("received: ", value);
-	client.emit('get_result', addon.factorial(parseInt(value)));
+	client.emit('get_factorial', addon.factorial(parseInt(value)));
 }
 
 
